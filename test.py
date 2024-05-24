4ee383ba1cad5dd41c87cc63de78fb5e58fd2f2e
moyan <1561515308@qq.com>
2024-05-24 15:41:08 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from alist import AList, AListUser
 
 app = AListAdmin("http://127.0.0.1:5244/")
-adm = AListUser("admin", "jdc20101217")
+adm = AListUser("admin", "12r5jhdgchghjkrttjnbg6gxnkuufj[p8tcr8ufyy")
 print(app.UserInfo())
 print(app.headers)
