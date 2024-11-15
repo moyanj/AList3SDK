0eb36109806ff3605439b14b2e84a6b5754b164a
moyan <1561515308@qq.com>
2024-11-15 18:15:14 +0800

    添加python版本限制

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version="1.3.1",
     description="AListV3 PythonSDK",
     author="MoYan",
+    author_email="moyanjdc@qq.com",
     packages=find_packages(),
     long_description=open("readme.md").read(),  # 包的详细描述
     long_description_content_type="text/markdown",  # 描述的内容类型
@@ -12,12 +13,19 @@ setup(
         # 添加你的依赖库
         "aiohttp",
     ],
+    python_requires=">=3.9",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Libraries",
         "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "Topic :: Utilities",
         "Natural Language :: English",
         "Natural Language :: Chinese (Simplified)"
